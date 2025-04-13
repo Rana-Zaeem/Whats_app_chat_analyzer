c63452956a6b099ff40cf98042b38c6097239871
zaeemrajpoot445@gmail.com <zaeemrajpoot445@gmail.com>
2025-04-13 12:54:33 +0500

    Add streamlined deployment configs

--- setup.sh
+++ setup.sh
@@ -1,14 +1,4 @@
 mkdir -p ~/.streamlit/
-mkdir -p ~/.nltk_data
 
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
-
-python -m nltk.downloader punkt
-python -m nltk.downloader stopwords
-python -m spacy download en_core_web_sm
+python -m nltk.downloader -d /app/nltk_data punkt stopwords
+python -m spacy download --no-deps en_core_web_sm
